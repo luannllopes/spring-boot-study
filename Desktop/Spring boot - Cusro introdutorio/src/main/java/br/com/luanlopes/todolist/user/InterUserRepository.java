a805e7f03d310f0164a66f8201cc82934db672a6
Luan Lopes <luannllopes@gmail.com>
2025-07-12 12:13:05 -0300

    Delete Desktop/Spring boot - Cusro introdutorio directory

--- Desktop/Spring boot - Cusro introdutorio/src/main/java/br/com/luanlopes/todolist/user/InterUserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.luanlopes.todolist.user;
-
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InterUserRepository extends JpaRepository<UserModel, UUID>{
-    UserModel findByUsername(String username);
-}
